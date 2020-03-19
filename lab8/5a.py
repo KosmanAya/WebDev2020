11adfb4305e4fe6c7c2f8259dcfdd73becb82c47
KosmanAya <kosmanaya@gmail.com>
2020-03-19 10:16:50 +0600

    lab8

---
+++ lab8/5a.py
@@ -0,0 +1,11 @@
+import end
+
+m = int(input())
+n = input().split()
+result = list(map(int, n))
+
+for i, j in enumerate(result):
+    if(i >= m):
+        break
+    if(i % 2 == 0):
+        print(j, end = ' ')
