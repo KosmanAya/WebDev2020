81436392f7cda06f612af2456111fe5ac53b38c4
KosmanAya <kosmanaya@gmail.com>
2020-04-17 10:46:17 +0600

    aa

--- hh_back/api/views.py
+++ hh_back/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from api.models import Company, Vacancy
 
+
 # company functions
 def company_list(request):
     try:
@@ -9,15 +10,17 @@ def company_list(request):
         companies_json = [c.to_json() for c in companies]
         return JsonResponse(companies_json, safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
+
 
 def company_detailed(request, id):
     try:
         company = Company.objects.get(id=id)
         return JsonResponse(company.to_json(), safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
-    
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
+
+
 def company_vacancies(request, id):
     try:
         company = Company.objects.get(id=id)
@@ -25,7 +28,7 @@ def company_vacancies(request, id):
         vacancies_json = [v.to_json() for v in vacancies]
         return JsonResponse(vacancies_json, safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
 
 
 # vacancy functions:
@@ -35,14 +38,16 @@ def vacancy_list(request):
         vacancies_json = [v.to_json() for v in vacancies]
         return JsonResponse(vacancies_json, safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
+
 
 def vacancy_detailed(request, id):
     try:
         vacancy = Vacancy.objects.get(id=id)
         return JsonResponse(vacancy.to_json(), safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
+
 
 def top_salaries(request):
     try:
@@ -50,5 +55,4 @@ def top_salaries(request):
         vacancies_json = [v.to_json() for v in vacancies]
         return JsonResponse(vacancies_json, safe=False)
     except:
-        return JsonResponse({ "error": "Something gone wrong while reading the database..." })
-            
+        return JsonResponse({"error": "Something gone wrong while reading the database..."})
